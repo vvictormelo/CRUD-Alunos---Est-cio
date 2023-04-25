aa1ee25f155ff12c43e01764af471a0336562146
Victor Melo <melovictor515@gmail.com>
2023-04-24 23:57:30 -0300

    implementação de validador de matrícula

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
 def program():
     print("\033[1;36m\nSEJA BEM VINDO...\n\033[m")
 
-    menuInicial()
-
-
-def menuInicial():
     acao = 0
     while acao != 6:
         print("-=" * 15)
-        print(f'{"CADASTRO ACADÊMICO":>24}')
+        print(f'{"MENU - CADASTRO ACADÊMICO":>28}')
         print("-=" * 15)
         print("\n1 - Cadastrar Aluno")
         print("2 - Alterar Aluno")
@@ -28,25 +24,31 @@ def menuInicial():
             acao = float(input("\nInsira a ação desejada: "))
             match acao:
                 case 1:
-                    cadastroAluno()  # falta cadastrar validação matrícula
+                    cadastroAluno()  # concluído
                 case 2:
                     alterarAluno()  # falta implementar
                 case 3:
-                    buscarAluno()  # concluído
+                    buscarAluno()  # lógica concluída. Inserir na exibição os dados de Configuração
                 case 4.1:
-                    listarAlunos()  # Ajustar exibição
+                    listarAlunos()  # Ajustar exibição e inserir a média
                 case 4.2:
-                    alunosAprovados()
+                    alunosAprovados()  # falta implementar
+                case 4.3:
+                    alunosReprovadosFaltas()  # falta implementar
+                case 4.4:
+                    alunoMaxMedia()  # falta implementar
+                case 4.5:
+                    alunoMinMedia()  # falta implementar
                 case 5:
-                    configuraDisciplina()
+                    configurarDisciplina()  # concluído
                 case 6:
                     acao = 6
                     print("Saindo...")
                     print("Até logo!!")
             if (acao > 6.0) or (acao < 1.0):
-                print("\033[1;31m\nOops... Ação inválida. Tente novamente.\033[m")
+                print("\033[1;31m\nOops... Ação inválida. Tente novamente!\n\033[m")
         except ValueError:
-            print("\033[1;31m\nOops... Ação inválida. Tente novamente.\033[m")
+            print("\033[1;31m\nOops... Ação inválida. Tente novamente!\n\033[m")
 
 
 def cadastroAluno():
@@ -55,10 +57,9 @@ def cadastroAluno():
             print("\n-=-=-=-=-=-=Cadastro de Aluno-=-=-=-=-=-=")
             matriculaAluno = int(input("\nMatrícula do aluno: "))
 
-            # for linha in dbalunos:
-            #    linha = linha.strip()
-            #    if matriculaAluno in linha:
-            #        print("Matrícula já cadastrada. Tente novamente: ")
+            validadorMatricula(
+                matriculaAluno
+            )  # Validação se matrícula já existe na base
 
             nomeAluno = str(input("Nome do aluno: "))
             nota1Aluno = float(input("1ª nota: "))
@@ -66,18 +67,36 @@ def cadastroAluno():
             nota3Aluno = float(input("3ª nota: "))
             qtdFaltas = int(input("Quantidade de faltas: "))
 
-            dbalunos.write("Matricula: " + str(matriculaAluno) + str("\n"))
-            dbalunos.write("Nome: " + nomeAluno + str("\n"))
-            dbalunos.write("Nota 1: " + str(nota1Aluno) + str("\n"))
-            dbalunos.write("Nota 2: " + str(nota2Aluno) + str("\n"))
-            dbalunos.write("Nota 3: " + str(nota3Aluno) + str("\n"))
-            dbalunos.write("Faltas: " + str(qtdFaltas) + str("\n"))
+            dbalunos.write(str(matriculaAluno) + str("\n"))
+            dbalunos.write(nomeAluno + str("\n"))
+            dbalunos.write(str(nota1Aluno) + str("\n"))
+            dbalunos.write(str(nota2Aluno) + str("\n"))
+            dbalunos.write(str(nota3Aluno) + str("\n"))
+            dbalunos.write(str(qtdFaltas) + str("\n"))
             dbalunos.write(str("-") + str("\n"))
         print("\033[1;32m\nAluno cadastrado com sucesso!\n\033[m")
     except PermissionError:
-        print(
-            "\033[1;31m\nVocê não possui permissão para acessar o arquivo. Contante o suporte.\033[m"
-        )
+        print("\033[1;31m\nVocê não possui permissão para acessar o arquivo.\033[m")
+
+
+def validadorMatricula(matriculaAluno):
+    try:
+        with open("dbalunos.txt", "r", encoding="utf-8") as dbalunos:
+            checker = True
+
+            while checker == True:
+                for i, v in enumerate(dbalunos):
+                    if v == str(matriculaAluno) + "\n":
+                        print(
+                            "\033[1;31m\nMatrícula já cadastrada. Tente novamente!\033[m"
+                        )
+                        matriculaAluno = int(input("\nMatrícula do aluno: "))
+                    else:
+                        checker = False
+    except FileNotFoundError:
+        print("\033[1;31mArquivo não encontrado!\033[m")
+    except PermissionError:
+        print("\033[1;31m\nVocê não possui permissão para acessar o arquivo.\033[m")
 
 
 def alterarAluno():
@@ -140,10 +159,27 @@ def listarAlunos():
         print("\033[1;31mVocê não possui credenciais para acessar o arquivo.\033[m")
 
 
-# def alunosAprovados():
+def alunosAprovados():
+    try:
+        print("\033[1;34m\n-=-=-=-=-=-=Aluno Aprovados-=-=-=-=-=-=\n\033[m")
+
+        with open("dbalunos.txt", "r", encoding="utf-8") as dbalunos:
+            for i, v in enumerate(dbalunos):
+                if v == matFind + "\n":
+                    aluno = list()
+                    aluno.append(matFind)
+                    for linha in dbalunos:
+                        if linha == "-\n":
+                            break
+                        else:
+                            aluno.append(linha.strip())
+    except FileNotFoundError:
+        print("\033[1;31mArquivo não encontrado!\033[m")
+    except PermissionError:
+        print("\033[1;31mVocê não possui credenciais para acessar o arquivo.\033[m")
 
 
-def configuraDisciplina():
+def configurarDisciplina():
     try:
         with open("config.txt", "w", encoding="utf-8") as configuracao:
             print("\n-=-=-=-=-=-=-=Configuração-=-=-=-=-=-=-=\n")
@@ -158,9 +194,7 @@ def configuraDisciplina():
             configuracao.write(str(anoDisciplina) + "\n")
             print("\033[1;32m\nDisciplina cadastrada com sucesso!\n\033[m")
     except PermissionError:
-        print(
-            "\033[1;31m\nVocê não possui permissão para acessar o arquivo. Contante o suporte.\033[m"
-        )
+        print("\033[1;31m\nVocê não possui permissão para acessar o arquivo.\033[m")
 
 
 if __name__ == "__main__":
--- testes.py
+++ testes.py
@@ -1,25 +1,27 @@
-print("\n-=-=-=-=-=-=Buscar Aluno-=-=-=-=-=-=\n")
-# matFind = str(input("Digite a matrícula: "))
-matFind = "202208646697"
+print("\033[1;34m\n-=-=-=-=-=-=Aluno Aprovados-=-=-=-=-=-=\n\033[m")
+# matriculaAluno = int(input("\nMatrícula do aluno: "))
 with open("dbalunos.txt", "r", encoding="utf-8") as dbalunos:
-    for i, v in enumerate(dbalunos):
-        if v == matFind + "\n":
-            aluno = list()
-            aluno.append(matFind)
-            for linha in dbalunos:
-                if linha == "-\n":
-                    break
-                else:
-                    aluno.append(linha.strip())
+    checker = True
 
-    print(
-        f"""\033[1;34m
-    Matrícula: {aluno[0]}
-    Nome: {aluno[1]}
-    Nota 1: {aluno[2]}
-    Nota 2: {aluno[3]}
-    Nota 3: {aluno[4]}
-    Média: {((float(aluno[2]) + float(aluno[3]) + float(aluno[4])) / 3):.2f}
-    Faltas: {aluno[5]}
-    \033[m"""
-    )
+    while checker == True:
+        matriculaAluno = int(input("\nMatrícula do aluno: "))
+        for i, v in enumerate(dbalunos):
+            if v == str(matriculaAluno) + "\n":
+                print("\033[1;31m\nMatrícula já cadastrada. Tente novamente!\n\033[m")
+                matriculaAluno = int(input("\nMatrícula do aluno: "))
+            else:
+                checker = False
+
+            # aluno = list()
+            # aluno.append(matFind)
+            # for linha in dbalunos:
+            #    if linha == "-\n":
+            #        break
+            #    else:
+            #        aluno.append(linha.strip())
+
+# if (((valor1 + valor2 + valor3) / 3) >= 6.0) and ((cargaHorariaDisciplina / qtdFaltas) >=)
+
+
+# 1º - Colocar os dados do db em uma lista
+# 2º - Trazer matrícula e nome do aluno aprovado, que tenha a média maior que
