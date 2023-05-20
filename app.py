20ccf59afd9322750cdbb6585590f53cefd4bf04
Victor Melo <melovictor515@gmail.com>
2023-05-19 21:49:48 -0300

    develop send

--- app.py
+++ app.py
@@ -24,11 +24,11 @@ def program():
             acao = float(input("\nInsira a ação desejada: "))
             match acao:
                 case 1:
-                    cadastroAluno()  # concluído
+                    cadastroAluno()
                 case 2:
                     alterarAluno()  # falta implementar
                 case 3:
-                    buscarAluno()  # lógica concluída. Inserir na exibição os dados de Configuração
+                    buscarAluno()
                 case 4.1:
                     listarAlunos()  # Ajustar exibição e inserir a média
                 case 4.2:
@@ -40,11 +40,12 @@ def program():
                 case 4.5:
                     alunoMinMedia()  # falta implementar
                 case 5:
-                    configurarDisciplina()  # concluído
+                    configurarDisciplina()
                 case 6:
                     acao = 6
-                    print("Saindo...")
+                    print("\nSaindo...")
                     print("Até logo!!")
+
             if (acao > 6.0) or (acao < 1.0):
                 print("\033[1;31m\nOops... Ação inválida. Tente novamente!\n\033[m")
         except ValueError:
@@ -55,11 +56,27 @@ def cadastroAluno():
     try:
         with open("dbalunos.txt", "a", encoding="utf-8") as dbalunos:
             print("\n-=-=-=-=-=-=Cadastro de Aluno-=-=-=-=-=-=")
-            matriculaAluno = int(input("\nMatrícula do aluno: "))
+            # matriculaAluno = int(input("\nMatrícula do aluno: "))
 
-            validadorMatricula(
-                matriculaAluno
-            )  # Validação se matrícula já existe na base
+            with open("dbalunos.txt", "r", encoding="utf-8") as dba:
+                alunos = []
+
+                for linha in dba.readlines():
+                    alunos.append(linha.strip("\n"))
+
+                checker = False
+
+                while checker == False:
+                    matriculaAluno = int(input("\nMatrícula do aluno: "))
+                    for item in alunos:
+                        if str(matriculaAluno) in item:
+                            print(
+                                "\033[1;31m\nMatrícula já inserida. Insira uma nova:\033[m"
+                            )
+                            matriculaAluno = int(input("\nMatrícula do aluno: "))
+                            checker = False
+                        elif str(matriculaAluno) not in item:
+                            checker = True
 
             nomeAluno = str(input("Nome do aluno: "))
             nota1Aluno = float(input("1ª nota: "))
@@ -67,45 +84,26 @@ def cadastroAluno():
             nota3Aluno = float(input("3ª nota: "))
             qtdFaltas = int(input("Quantidade de faltas: "))
 
-            dbalunos.write(str(matriculaAluno) + str("\n"))
-            dbalunos.write(nomeAluno + str("\n"))
-            dbalunos.write(str(nota1Aluno) + str("\n"))
-            dbalunos.write(str(nota2Aluno) + str("\n"))
-            dbalunos.write(str(nota3Aluno) + str("\n"))
-            dbalunos.write(str(qtdFaltas) + str("\n"))
-            dbalunos.write(str("-") + str("\n"))
+            aluno = [
+                matriculaAluno,
+                nomeAluno,
+                nota1Aluno,
+                nota2Aluno,
+                nota3Aluno,
+                qtdFaltas,
+            ]
+
+            dbalunos.write(str(aluno) + ";" + str("\n"))
+
         print("\033[1;32m\nAluno cadastrado com sucesso!\n\033[m")
     except PermissionError:
         print("\033[1;31m\nVocê não possui permissão para acessar o arquivo.\033[m")
 
 
-def validadorMatricula(matriculaAluno):
-    try:
-        with open("dbalunos.txt", "r", encoding="utf-8") as dbalunos:
-            checker = True
-
-            while checker == True:
-                for i, v in enumerate(dbalunos):
-                    if v == str(matriculaAluno) + "\n":
-                        print(
-                            "\033[1;31m\nMatrícula já cadastrada. Tente novamente!\033[m"
-                        )
-                        matriculaAluno = int(input("\nMatrícula do aluno: "))
-                    else:
-                        checker = False
-    except FileNotFoundError:
-        print("\033[1;31mArquivo não encontrado!\033[m")
-    except PermissionError:
-        print("\033[1;31m\nVocê não possui permissão para acessar o arquivo.\033[m")
-
-
 def alterarAluno():
-    print("\n-=-=-=-=-=-=Alunos Cadastrados-=-=-=-=-=-=")
+    print("\n-=-=-=-=-=-=Alterar Aluno-=-=-=-=-=-=")
     try:
-        with open("dbalunos.txt", "r", encoding="utf-8") as dbalunos:
-            for linha in dbalunos.readlines():
-                print(linha.strip("\n"))
-
+        
     except PermissionError:
         print(
             "\033[1;31mVocê não possui credenciais para acessar o arquivo.\033[m"
@@ -117,34 +115,36 @@ def alterarAluno():
 def buscarAluno():
     try:
         print("\033[1;34m\n-=-=-=-=-=-=Buscar Aluno-=-=-=-=-=-=\n\033[m")
-        matFind = str(input("Digite a matrícula: "))
+        matFind = str(input("\033[1;34mDigite a matrícula: \033[m"))
 
         with open("dbalunos.txt", "r", encoding="utf-8") as dbalunos:
-            for i, v in enumerate(dbalunos):
-                if v == matFind + "\n":
-                    aluno = list()
-                    aluno.append(matFind)
-                    for linha in dbalunos:
-                        if linha == "-\n":
-                            break
-                        else:
-                            aluno.append(linha.strip())
+            aluno = []
+            for linha in dbalunos.readlines():
+                if matFind in linha:
+                    aluno.append(
+                        linha.strip("\n").strip("[").strip(";").strip("]").strip("'")
+                    )
+
+            for alun in aluno:
+                listAluno = alun.split(", ")
 
         print(
             f"""\033[1;34m
-        Matrícula: {aluno[0]}
-        Nome: {aluno[1]}
-        Nota 1: {aluno[2]}
-        Nota 2: {aluno[3]}
-        Nota 3: {aluno[4]}
-        Média: {((float(aluno[2]) + float(aluno[3]) + float(aluno[4])) / 3):.2f}
-        Faltas: {aluno[5]}
+        Nome: {listAluno[1]}
+        Nota 1: {listAluno[2]}
+        Nota 2: {listAluno[3]}
+        Nota 3: {listAluno[4]}
+        Média: {((float(listAluno[2]) + float(listAluno[3]) + float(listAluno[4])) / 3):.2f}
+        Faltas: {listAluno[5]}
         \033[m"""
         )
+
     except FileNotFoundError:
-        print("\033[1;31mArquivo não encontrado!\033[m")
+        print("\033[1;31m\nArquivo não encontrado!\033[m")
     except PermissionError:
-        print("\033[1;31mVocê não possui credenciais para acessar o arquivo.\033[m")
+        print("\033[1;31m\nVocê não possui credenciais para acessar o arquivo.\033[m")
+    except UnboundLocalError:
+        print("\033[1;31m\nMatrícula não existente na base.\033[m")
 
 
 def listarAlunos():
@@ -181,7 +181,7 @@ def alunosAprovados():
 
 def configurarDisciplina():
     try:
-        with open("config.txt", "w", encoding="utf-8") as configuracao:
+        with open("config.txt", "x", encoding="utf-8") as configuracao:
             print("\n-=-=-=-=-=-=-=Configuração-=-=-=-=-=-=-=\n")
             nomeDisciplina = str(input("Nome da disciplina: "))
             nomeProfessor = str(input("Nome do professor: "))
@@ -195,6 +195,8 @@ def configurarDisciplina():
             print("\033[1;32m\nDisciplina cadastrada com sucesso!\n\033[m")
     except PermissionError:
         print("\033[1;31m\nVocê não possui permissão para acessar o arquivo.\033[m")
+    except FileExistsError:
+        print("\033[1;31m\nArquivo de configuração já criado e configurado.\033[m")
 
 
 if __name__ == "__main__":
